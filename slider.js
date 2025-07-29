4243e8ae97af1ddc80b196c24d8f30cc3765d6e0
Denis-Sl-py <pnas200@mail.ru>
2025-07-29 12:20:09 +0300

    Update slider.js

--- slider.js
+++ slider.js
@@ -1,9 +1,9 @@
 const images = [
-  "https://yandex.ru/images/search?pos=0&from=tabbar&img_url=https%3A%2F%2Fcs6.pikabu.ru%2Fpost_img%2Fbig%2F2015%2F06%2F08%2F3%2F1433735650_472905306.jpg&text=картинки&rpt=simage&lr=213",
-  "https://yandex.ru/images/search?pos=2&from=tabbar&img_url=https%3A%2F%2Fsteamuserimages-a.akamaihd.net%2Fugc%2F52453354080448818%2F543783B601D5A853E3F50907B9722A314DFD92B6%2F%3Fimw%3D512%26amp%3Bimh%3D320%26amp%3Bima%3Dfit%26amp%3Bimpolicy%3DLetterbox%26amp%3Bimcolor%3D%2523000000%26amp%3Bletterbox%3Dtrue&text=картинки&rpt=simage&lr=213",
-  "https://yandex.ru/images/search?pos=10&from=tabbar&img_url=https%3A%2F%2Fimg3.akspic.ru%2Fattachments%2Foriginals%2F7%2F1%2F2%2F0%2F0%2F100217-vodnye_elementy-vodopad-voda-vodotok-rastitelnost-2880x1800.jpg&text=картинки&rpt=simage&lr=213",
-  "https://yandex.ru/images/search?pos=22&from=tabbar&img_url=https%3A%2F%2Fw-dog.ru%2Fwallpapers%2F9%2F19%2F500841690050007%2Fitaliya-gory-ozero-ostrov-les.jpg&text=картинки&rpt=simage&lr=213",
-  "https://yandex.ru/images/search?pos=29&from=tabbar&img_url=https%3A%2F%2Fplay-lh.googleusercontent.com%2FSmptFHWgOWbBTVdme_FZMygqnyGY2BVo7fwLvc40tLcSoAR9SOsfULspvFQaXFPAkTQ%3Dw7680-h4320&text=картинки&rpt=simage&lr=213",
+  "i1.jpg",
+  "i2.jpg",
+  "i3.jpg",
+  "i4.jpg",
+  "i5.jpg"
 ];
 
 let index = 0;
